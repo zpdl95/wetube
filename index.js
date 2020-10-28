da03a5d40f137a0ea617ab5414722ff3048b1471
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2020-10-28 23:09:50 +0900

    #2.6 Express Core: Middlewares
    nodemon --delay 추가

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const handleListening = () => console.log(`Listening on: https://localhost:${POR
 const handleHome = (req, res) => res.send("hello from home");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
+/*middlewares 첫요청과 마지막반응 사이에 존재하는것*/
+/*next인자가 필요함 전달용*/
+const betweenHome = (req, res, next) => {
+    console.log('between');
+    next();
+}
+
+app.use(betweenHome);
 
 app.get("/", handleHome);
 
