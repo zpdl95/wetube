9163db3c4da2a74aa917d17177b75e34879021e8
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2020-10-29 21:14:48 +0900

    #2.9 MVC Pattern part One
    M data
    V how does the data look
    C funtion that looks for the data

---
+++ routers/globalRouter.js
@@ -0,0 +1,6 @@
+import express from 'express';
+/*export로 userRouter만 내보내는것*/
+const globalRouter = express.Router();
+
+
+export default globalRouter;
---
+++ routers/videoRouter.js
@@ -0,0 +1,6 @@
+import express from 'express';
+/*export로 userRouter만 내보내는것*/
+const userRouter = express.Router();
+
+
+export default userRouter;
