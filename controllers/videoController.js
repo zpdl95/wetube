d3a4c32252e22f321a4e16b33fd8bda462b82855
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2020-11-01 20:21:17 +0900

    #2.18 Search Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,18 @@
-export const home = (req, res) => res.render("Home", {pageTitle: "Home"});
-export const search = (req, res) => res.render("Search", {pageTitle: "Search"});
+export const home = (req, res) => res.render("Home", {pageTitle: "Home"})
+
+export const search = (req, res) => {
+    // const searchingBy = req.query.term; /* 구 버전 방식 */
+    const {query:{term:searchingBy}} = req;
+    // {pageTitle: "Search", searchingBy: searchingBy} /* 구버전 방식 */
+    res.render("search", {pageTitle: "Search", searchingBy});
+}
+
 export const videos = (req, res) => res.render("videos", {pageTitle: "Videos"});
+
 export const upload = (req, res) => res.render("upload", {pageTitle: "Upload"});
+
 export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle: "Video Detail"});
+
 export const editVideo = (req, res) => res.render("editVideo", {pageTitle: "Edit Video"});
+
 export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle: "Delete Video"});
