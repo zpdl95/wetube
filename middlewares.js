de8191605846ad9858b078ac98c76419b9d74b17
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2020-11-12 20:43:12 +0900

    #5.1 Footer, Login & Join

--- middlewares.js
+++ middlewares.js
@@ -5,7 +5,7 @@ export const localsMiddlewares = (req, res, next) => {
   res.locals.siteName = "WeTube";
   res.locals.routes = routes;
   res.locals.user = {
-    isAuthenticated: true,
+    isAuthenticated: false,
     id: 1,
   };
   next();
